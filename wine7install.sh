35d3148f22f1db9c92acb79c941ba8f8f672a411
Nick Matthews <matthews3102000@gmail.com>
2022-01-27 03:16:02 -0500

    wine7install.sh
    This is the install script for wine version 7.0

---
+++ wine7install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#This enables 32bit architecture
+sudo dpkg --add-architecture i386
+
+#This downloads the security key
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+
+#This adds the key to the keys list
+sudo apt-key add winehq.key
+
+#This adds the qinehq repository for version 7 to the repository list
+sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
+
+#This updates everything
+sudo apt update
+
+#This installs wine version 7
+sudo apt install --install-recommends winehq-stable
+
+#Subject to GPLv3 License Copyright Nicholas Matthews 2022
