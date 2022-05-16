68ed351eb9684e65b97735b05f01cd9176e81489
Nick Matthews <matthews3102000@gmail.com>
2022-05-16 16:09:59 -0400

    Updated scripts!
    Because the way Wine needs to be installed has changed I am now updating my older scripts to reflect this. I will make the changes to Mint and Debian soon™

--- wine7install.sh
+++ wine7install.sh
@@ -6,16 +6,22 @@ sudo dpkg --add-architecture i386
 #This downloads the security key
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
 
-#This adds the key to the keys list
-sudo apt-key add winehq.key
+#This moves the security key to the correct place
+sudo mv winehq.key /usr/share/keyrings/winehq-archive.key
+
+#This adds WineHQ repository to the sources list
+wget -nc https://dl.winehq.org/wine-builds/ubuntu/dists/focal/winehq-focal.sources
 
 #This adds the winehq repository for version 7 to the repository list
-sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
+sudo mv winehq-focal.sources /etc/apt/sources.list.d
 
 #This updates everything
 sudo apt update
 
+#This upgrades the packages
+sudo apt upgrade
+
 #This installs wine version 7
 sudo apt install --install-recommends winehq-stable
 
-#Subject to GPLv3 License Copyright Nicholas Matthews 2022
+#Subject to GPLv3 License Copyright Nicolas Matthews 2022
