aa73947ec0f35b0ea025522d72054da834aa3e2e
Nick Matthews <matthews3102000@gmail.com>
2022-05-17 00:36:22 -0400

    Updated the Debian 11 version

--- Wine7InstallDebian11.sh
+++ Wine7InstallDebian11.sh
@@ -3,19 +3,25 @@
 #This enables 32bit architecture
 sudo dpkg --add-architecture i386
 
-#This downloads the security key
+#This downloads the repository key
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
 
-#This adds the key to the keys list
-sudo apt-key add winehq.key
+#This adds the key to the keyring
+sudo mv winehq.key /usr/share/keyrings/winehq-archive.key
 
 #This adds the winehq repository for version 7 to the repository list
-sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/debian/ bullseye main'
+wget -nc https://dl.winehq.org/wine-builds/debian/dists/bullseye/winehq-bullseye.sources
 
-#This updates everything
+#This moves the source to the sources list
+sudo mv winehq-bullseye.sources /etc/apt/sources.list.d
+
+#This installs any updates
 sudo apt update
 
-#This installs wine version 7
+#This upgrades any available packages
+sudo apt upgrade
+
+#This installs the wine package
 sudo apt install --install-recommends winehq-stable
 
 #Subject to GPLv3 License Copyright Nicholas Matthews 2022
